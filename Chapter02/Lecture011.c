0afcec6392187d01690f81e52a1ae80d0eca9502
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-27 21:55:22 +0530

    Created First Commit for Lecture 11

---
+++ Chapter02/Lecture011.c
@@ -0,0 +1,22 @@
+// Lecture 11.2 Discount on Spl Purchase
+#include<stdio.h>
+
+int main(){
+    int qty,dis=0;
+    float rate,tot;
+
+    printf("Enter Qty:");
+    scanf("%d",&qty);
+    printf("Enter Rate :");
+    scanf("%f",&rate);
+
+
+    if (qty>1000)
+        dis=10;
+    tot=(qty*rate)-(qty*rate*dis/100);
+        //print("Enjoy the shopping");
+    //else
+    printf("Total Expence with discount : %f",tot);
+
+    return 0;
+}
