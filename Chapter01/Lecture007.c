45ab2c9086b5708c3cf31e062ddd25201f9e7f8f
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-27 17:13:09 +0530

    First Commit for Lecture 7

---
+++ Chapter01/Lecture007.c
@@ -0,0 +1,11 @@
+//Lecture 7.1 Area of a Square
+ #include<stdio.h>
+ int main(){
+    int side,AreaofSquare;
+    printf("Enter Side of a Square : ");
+    scanf("%d",&side);
+    AreaofSquare=side*side; 
+    printf("Area of Square: %d",AreaofSquare);
+
+    return 0;
+}
