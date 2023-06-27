58700a44a9a113091ae2b2b63e5e2769da292187
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-27 19:07:47 +0530

    Updated Commit for Lecture 9

--- Chapter01/Lecture009.c
+++ Chapter01/Lecture009.c
@@ -1,23 +1,24 @@
-/*Lecture 9 Find out Volume, LSA and TSA of Cylinder. */
+/*Lecture 9 
+Cylinder,Cone,Conic etc. */
 //Volume of Cylinder = pi*r*r*h
 //LSA of Cylinder = 2*pi*r*h 
 //TSA of Cylinder = 2*pi*r(r+h)
 #include<stdio.h>
 
 int main(){
-    int radius,Height,pi=3.14,VolumeofCylinder,LSAofCylinder,TSAofCylinder;
+    float radius,Height,pi=3.14,VolumeofCylinder,LSAofCylinder,TSAofCylinder;
     printf("Enter the Radius of Cylinder :");
-    scanf("%d",&radius);
+    scanf("%f",&radius);
     printf("Enter the Height of Cylinder :");
-    scanf("%d",&Height);
+    scanf("%f",&Height);
 
     VolumeofCylinder = pi*radius*radius*Height;
     LSAofCylinder = 2*pi*radius*Height;
     TSAofCylinder = 2*pi*radius*(radius+Height);
 
-    printf("Volume of Cylinder :%d ",VolumeofCylinder);
-    printf("\nLateral Surface Area of Cylinder :%d ",LSAofCylinder);
-    printf("\nTotal Surface Area of Cylinder :%d ",TSAofCylinder);
+    printf("Volume of Cylinder :%f ",VolumeofCylinder);
+    printf("\nLateral Surface Area of Cylinder :%f ",LSAofCylinder);
+    printf("\nTotal Surface Area of Cylinder :%f ",TSAofCylinder);
 
     return 0;
 }
