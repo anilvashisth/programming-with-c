e85f5956e3b346be48c6e1cf56fe9b8ed468cb12
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-28 13:54:20 +0530

    First Commit for Program 27

---
+++ Chapter03/Program027.c
@@ -0,0 +1,8 @@
+//Program 27 Print 1 to 100 only Even Number using For loop
+# include<stdio.h>
+int main(){
+    for(int i=1;i<=100;i++)
+        if(i%2==0)
+        printf("%d ",i);
+    return 0;
+}
