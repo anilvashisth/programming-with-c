2bb530af056c78beb090f12292ec9e4add8a00ab
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-28 10:27:03 +0530

    First Commit for Program 25

---
+++ Chapter02/Program025.c
@@ -0,0 +1,11 @@
+//Program 25 Take print msg using Loop.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the no :");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i=i+2){
+        printf("Sare Jahaan se Achacha Hindustaan Hamara\n");
+    }
+    return 0;
+}
