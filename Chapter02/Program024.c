34d804f46f7edaf567aac4a24ac7743968afedc9
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-28 10:23:34 +0530

    First Commit for Program 24

---
+++ Chapter02/Program024.c
@@ -0,0 +1,11 @@
+// Program 24 Print the Message Many times
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the Number :");
+    scanf("%d",&num);
+    for(int i=1;i<=5;i++){
+        printf("Sare Jahaan se Achacha Hindustaan Hamara,Hamara\n");
+    }
+    return 0;
+}
