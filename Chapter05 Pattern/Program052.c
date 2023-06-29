bf3a432d2b75e0a089d99a5d4c187572f450b089
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-29 21:49:57 +0530

    First Commit Program 52

---
+++ Chapter05 Pattern/Program052.c
@@ -0,0 +1,16 @@
+//Program 52
+//Pattern Program 3
+#include<stdio.h>
+int main(){
+    int i,j;
+    for (i=1;i<=6;i++)
+    {
+       for(j=1;j<=6;j++)
+        {
+            printf(" * ");
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
