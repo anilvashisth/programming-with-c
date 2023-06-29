2a9c8acefd81bbce2c74dffdc0f00846b3753498
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-29 21:53:32 +0530

    First Commit Program 53

---
+++ Chapter05 Pattern/Program053.c
@@ -0,0 +1,16 @@
+//Program 53
+//Pattern Program 4
+#include<stdio.h>
+int main(){
+    int i,j;
+    for (i=1;i<=7;i++)
+    {
+       for(j=1;j<=7;j++)
+        {
+            printf("%c ",j+64);
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
