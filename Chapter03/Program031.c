ad9c5635327073cea5ae6942bc9dc716a99f6174
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-29 13:35:16 +0530

    Create First Commit Program 31

---
+++ Chapter03/Program031.c
@@ -0,0 +1,12 @@
+//Lecture 31
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the no :");
+    scanf("%d",&n);
+    for(int i=4;i<=3*(n+1);i=i+3)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}
