65d75de1b75dfcbf3d4e1ea6e0b88e57ea13b1bc
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-30 07:39:06 +0530

    First Commit Program 60

---
+++ Chapter05 Pattern/Program060.c
@@ -0,0 +1,16 @@
+//Program60
+//Pattern Program 11 Print Triangle
+#include<stdio.h>
+int main(){
+    int i,j;
+    for (i=1;i<=5;i++)
+    {
+       for(j=1;j<=i;j++)
+        {
+            printf(" !@#$%^&* ");
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
