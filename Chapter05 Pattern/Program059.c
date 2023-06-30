e1186ea6a51ce99a218579e5013c10094fb0a590
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-30 07:20:29 +0530

    First Commit Program059

---
+++ Chapter05 Pattern/Program059.c
@@ -0,0 +1,16 @@
+//Program 59
+//Pattern Program 10 Print Triangle
+#include<stdio.h>
+int main(){
+    int i,j;
+    for (i=1;i<=5;i++)
+    {
+       for(j=1;j<=i;j++)
+        {
+            printf(" A ");
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
