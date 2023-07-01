b0f640bbd0e10c67b44a81029f08062ffafcfecd
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-01 21:15:01 +0530

    First Commit Program 67

---
+++ Chapter06 Pattern/Program067.c
@@ -0,0 +1,20 @@
+//Program 67
+//Pattern Program 17
+1 2 3 4 5 
+1 2 3 4
+1 2 3
+1 2
+1
+#include<stdio.h>
+int main(){
+    for(int i=5;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+        printf("%d ",j);
+        }
+    printf("\n");
+      
+    }
+    return 0;
+}
