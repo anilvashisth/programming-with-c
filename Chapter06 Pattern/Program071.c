f4111186b6daa80c138142474630e996fb9befd4
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-01 21:52:41 +0530

    First Commit Program 71

---
+++ Chapter06 Pattern/Program071.c
@@ -0,0 +1,19 @@
+//Pattern Program 21
+* * * * * 
+* * * *
+* * *
+* *
+*
+
+#include<stdio.h>
+int main(){
+    for(int i=5;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+        printf("* ");
+        }
+    printf("\n");
+    }
+    return 0;
+}
