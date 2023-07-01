c7436c4f4795a2f3caab53a0c0841e8f5ebfa731
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-01 21:22:56 +0530

    First Commit Program 69

---
+++ Chapter06 Pattern/Program069.c
@@ -0,0 +1,19 @@
+Program 69
+//Pattern Program 19
+1 1 1 1 1 
+2 2 2 2 
+3 3 3 
+4 4 
+5 
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=i;j<=5;j++)
+        {
+        printf("%d ",i);
+        }
+    printf("\n");
+    }
+    return 0;
+}
