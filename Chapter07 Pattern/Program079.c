fd15a59b52466353eaadd6ef5e3172cf3e780015
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-03 16:15:46 +0530

    First Commit Program 79

---
+++ Chapter07 Pattern/Program079.c
@@ -0,0 +1,23 @@
+//Program 79
+//Pattern Program 29
+     1
+    12
+   123
+  1234
+ 12345
+     
+#include<stdio.h>
+int main(){
+        int i,j,k;
+    for(int i=1;i<=5;i++)
+    {
+        for(int k=5;k>=i;k--)
+            printf(" ");
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+    printf("\n");
+    }
+    return 0;
+}
