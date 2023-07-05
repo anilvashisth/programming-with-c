2610ecbfde857d65567ec13ebafc79c77e1f03be
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-05 18:51:29 +0530

    Update Commit Program 81

--- Chapter08Array/Program081.c
+++ Chapter08Array/Program081.c
@@ -1,5 +1,20 @@
+//Program 81 WAP to Print Marks and Age of a Strudent using Array
 #include<stdio.h>
 int main(){
-printf("Hello Learner, This is DSA Series,'Array Lecture01'");
-  return 0;
+    int marks[5]= {15,25,21,23,19};
+    int age[]={14,15,16,14,13};
+
+    printf("Marks:%d ",marks[0]);
+    printf("%d ",marks[1]);
+    printf("%d ",marks[2]);
+    printf("%d ",marks[3]);
+    printf("%d ",marks[4]);
+    printf("\nAge : %d ",age[0]);
+    printf("%d ",age[1]);
+    printf("%d ",age[2]);
+    printf("%d ",age[3]);
+    printf("%d ",age[4]);
+    printf("\nHello Learner, This is DSA Array Series ,Program 1");
+
+    return 0;
 }
