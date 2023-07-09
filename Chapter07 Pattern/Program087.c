a44f7589a2f852d4183a942867e10da453d1cec8
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-09 21:25:46 +0530

    First Commit Program 87

---
+++ Chapter07 Pattern/Program087.c
@@ -0,0 +1,41 @@
+// Program 87 About Pattern Program using c
+/*
+The No of Lines: 10
+*********ABCDEFGHIJIHGFEDCBA
+********ABCDEFGHIHGFEDCBA
+*******ABCDEFGHGFEDCBA
+******ABCDEFGFEDCBA
+*****ABCDEFEDCBA
+****ABCDEDCBA
+***ABCDCBA
+**ABCBA
+*ABA
+A
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("The No of Lines: ");
+    scanf("%d",&n);
+    int nsp=n-1;
+        
+    for (int i=n;i>=1;i--){
+        int a=i-1;
+        for(int q=1;q<=nsp;q++){
+            printf("*");
+        }
+        nsp--;
+        for (int j=1;j<=i;j++){
+            char ch =(char)( j+64);
+            printf("%c",ch);
+        }
+        for (int k=1;k<=i-1;k++){
+            char ch = (char)(a+64);
+            printf("%c",ch);
+            a--;
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
