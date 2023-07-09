0801e6a6987c0fd9d1cf40a7c03d99f62bd18e33
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-09 18:01:21 +0530

    Update Commit Program 84

--- Chapter07 Pattern/Program084.c
+++ Chapter07 Pattern/Program084.c
@@ -1,7 +1,36 @@
-#incliude<stdio.h>
+// Program 84 About Pattern Program using c
+/*
+The No of Lines: 5
+    A
+   ABA
+  ABCBA
+ ABCDCBA
+ABCDEDCBA
+*/
+#include<stdio.h>
 int main(){
-printf("Hello C Language");
+    int n;
+    printf("The No of Lines: ");
+    scanf("%d",&n);
+    int nsp=n-1;
+        
+    for (int i=1;i<=n;i++){
+        int a=i-1;
+        for(int q=1;q<=nsp;q++){
+            printf(" ");
+        }
+        nsp--;
+        for (int j=1;j<=i;j++){
+            char ch =(char)( j+64);
+            printf("%c",ch);
+        }
+        for (int k=1;k<=i-1;k++){
+            char ch = (char)(a+64);
+            printf("%c",ch);
+            a--;
 
-
-  return 0;
+        }
+        printf("\n");
+    }
+    return 0;
 }
